63ed7b401664c97d220dc63744ebfb2b499c6234
_wf_nik_ <momopopo659@gmail.com>
2024-01-28 09:26:48 +0300

    Commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# WF-JustMessenger (Rest + WebSocket)
+**Just messenger on Java/Spring** 
+
+## Front:
+**Frontend made: [zwanter](https://github.com/zwanter)**
+
+Not all functions have been added to the frontend that are
+
+## Stack: 
+1. Java
+2. Spring framework
+3. MongoDB
+4. Minio
+5. Elasticsearch
+6. Docker
+7. JWT + RT
+
+
+## Screens:
+![Screen_1](C:\Users\momop\IdeaProjects\WF-JustMessenger\img\img_0.png)
+![Screen_2](C:\Users\momop\IdeaProjects\WF-JustMessenger\img\img_1.png)
+![Screen_3](C:\Users\momop\IdeaProjects\WF-JustMessenger\img\img_2.png)
+![Screen_4](C:\Users\momop\IdeaProjects\WF-JustMessenger\img\img_3.png)
+![Screen_5](C:\Users\momop\IdeaProjects\WF-JustMessenger\img\img_4.png)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- front/src/main/java/wf/spring/justmessenger_front/controller/IndexController.java
+++ front/src/main/java/wf/spring/justmessenger_front/controller/IndexController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -12,17 +14,17 @@ public class IndexController {
 
     @GetMapping
     public String index() {
-        return  "forward:/index.html";
+        return "forward:/index.html";
     }
 
     @GetMapping("/login")
     public String login() {
-        return  "forward:/login.html";
+        return "forward:/login.html";
     }
 
     @GetMapping("/register")
     public String singup() {
-        return  "forward:/signup.html";
+        return "forward:/signup.html";
     }
 
 
